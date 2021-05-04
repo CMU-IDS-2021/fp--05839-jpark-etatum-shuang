3cfe856c31fadd639b3d304ceb21d7e443b8a6d9
HuangShiCheng <shichenh@andrew.cmu.edu>
2021-05-04 21:19:24 +0800

    commit
    Change-Id: Iccadf9899fbb2957f0a4926b9ee813ea312cc32b

--- src/heatmap.py
+++ src/heatmap.py
@@ -10,6 +10,9 @@ def heatmap_chart():
 	# generate second chart
 	st.markdown("Now comes the interesting part, what are the occupancy rate of Airbnb housing? "
 				"The following heatmap will tell you about it.")
+	st.markdown("The heatmap below shows a obvious trend of drop in occupancy rate. "
+				"Especially the comparision between the year 2015/2016 and 2020/2021, heatmap are much more yellow in "
+				"previous years and blue in recent years, which could be the outcome of Covid-19.")
 	chart1 = generate_heatmap()
 	st.plotly_chart(chart1)
 
